655409006fd59f21145ff343b3b133df27b1e75d
ekam@getinkworks.com <ekam@getinkworks.com>
2023-09-26 13:19:44 -0700

    type policies

--- 04-infinite-scroll-pagination/web/src/workshop/apollo/typePolicies.ts
+++ 04-infinite-scroll-pagination/web/src/workshop/apollo/typePolicies.ts
@@ -10,4 +10,20 @@ export const typePolicies: TypePolicies = {
   Player: {
     keyFields: [],
   },
+  Playlist: {
+    fields: {
+      tracks: {
+        keyArgs: false,
+        merge: (existing, incoming, { mergeObjects }) => {
+          const existingEdges = existing?.edges ?? []
+          const result = {
+            __typename: incoming.__typename,
+            edges: [...existingEdges, ...incoming.edges],
+            pageInfo: mergeObjects(existing?.pageInfo, incoming.pageInfo),
+          }
+          return result
+        },
+      },
+    },
+  },
 }
